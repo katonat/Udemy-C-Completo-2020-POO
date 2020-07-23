5a0f0c7ca036c23750597225524c6e49d524ed1d
Nathalie Kato <nath.nicie@gmail.com>
2020-07-23 19:41:05 -0300

    constructors, overload and encapsulation

---
+++ Primeiro/Primeiro/Conta.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro {
+
+    public class Conta {
+
+        public int Numero { get; private set; }
+        public double Saldo { get; private set; }
+        public string _titular;
+
+
+        public Conta(int numero, string titular) {
+            Numero = numero;
+            this.SetTitular(titular);
+        }
+
+        public Conta(int numero, string titular, double deposito) : this(numero, titular) {
+            this.Depositar(deposito);
+        }
+
+        public string GetTitular() {
+            return _titular;
+        }
+
+        public void SetTitular(string nome) {
+            if(nome != null && nome.Length > 2) {
+                _titular = nome;
+            }
+        }
+
+        public void Depositar(double valor) {
+            if(valor > 0)
+                Saldo += valor;
+        }
+
+        public void Sacar(double valor) {
+            Saldo -= (valor+5);
+        }
+
+        public override string ToString() {
+            return "Conta "
+                + Numero
+                + ", "
+                + "Titular: "
+                + _titular
+                + ", "
+                + "Saldo R$ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ Primeiro/Primeiro/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial? (s/n)");
+            char deposito = char.Parse(Console.ReadLine());
+            double valor = 0.0;
+            if(deposito == 's') {
+                Console.WriteLine("Entre o valor de depósito inicial");
+                valor = double.Parse(Console.ReadLine());
+            }
+
+            Conta c1 = new Conta(numero, titular, valor);
+
+            Console.WriteLine("\nDados da Conta:");
+            Console.WriteLine(c1 + "\n");
+
+            Console.Write("Entre com um valor para depósito: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+            c1.Depositar(valorDeposito);
+            Console.WriteLine("Dados da Conta atualizados:");
+            Console.WriteLine(c1 + "\n");
+
+            Console.Write("Entre com um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+            c1.Sacar(valorSaque);
+            Console.WriteLine("Dados da Conta atualizados:");
+            Console.WriteLine(c1);
+
+        }
+    }
+}
